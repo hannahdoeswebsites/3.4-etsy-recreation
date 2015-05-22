97438f464fc0684ae028db7fcfa70e943ad21f25
Hannah Jordan <hjorda0126@aol.com>
2015-05-22 12:08:06 -0400

    Lots of Javascript changes. Images are shown. Shows seller and price. getting layout all put together.

--- js/main.js
+++ js/main.js
@@ -1,70 +1,59 @@
-(function(){
-  'use strict';
-
-  var url = 'https://api.etsy.com/v2/listings/active.js?api_key=smwj0zbdutofwc3mo9ptjui8&keywords=curiosities+and+oddities&includes=Images,Shop&limit=24&sort_on=score';
-  fetchJSONP(url, app);
-
-  /*
-    Call this function with the URL where the JSON lives.
-    We will pass a function as the second argument.
-    That function will be called when the request finished.
-    The argument to that function will be the JSON data.
-    You will need to change the values for url.
-  */
-
-  var ulElement = document.querySelector('.itemsForSale');
-
-  function fetchJSONP(url, callback) {
-      var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
-      var script = document.createElement('script');
-
-      window[callbackName] = function(data) {
-          delete window[callbackName];
-          document.body.removeChild(script);
-          callback(data);
-      };
-
-      script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
-      document.body.appendChild(script);
-  }
-
-  /*
-    Etsy's API return data in a slightly different format.
-    Extract the data accordingly
-  */
-
-  function app(response) {
-
-    var items = response.url_fullxfull;
-    console.log(images);
-    displayImages(images);
-  }
 
 
-  function displayImages(images) {
-    var source = document.querySelector("#itemsForSale-Template").innerHTML;
-    var template = Handlebars.compile(source);
-    items.forEach(function(image){
-      var output = template(image);
-      ulElement.insertAdjacentHTML('beforeend', output);
-    });
-  }
+    (function(){
+        var $productElement = document.querySelector(".itemsForSale");
+        var url = "https://api.etsy.com/v2/listings/active.js?api_key=smwj0zbdutofwc3mo9ptjui8&keywords=curiosities+and+oddities&includes=Images,Shop&limit=24&sort_on=score";
+        fetchJSONP(url, app);
+
+        function app(httpReturn){
+            var products = httpReturn.results;
+
+            displayProducts(products);
+        }
 
 
-  function app(response) {
+        function displayProducts(products){
+            console.log(products);
+            var shopname   = document.querySelector("#itemsForSale-Template").innerHTML;
+            var template = Handlebars.compile(shopname);
+            Handlebars.registerHelper('productImage', function(){
+                return this.Images[0].url_170x135;
+            } );
+            Handlebars.registerHelper('checkTitle', function(string){
+                //console.log(string);
+                if(string.length > 30){
+                    var s = string.slice(0,30);
+                    return s + "...";
+                }else return string;
+            });
 
-    var items = response.results;
-    console.log(items);
-    displayItemsForSale(items);
-  }
+            //console.log(html);
+            products.forEach(function(info){
+                var html = template(info);
+
+                $productElement.insertAdjacentHTML('beforeend',html);
+
+            });
+        }
+
+        function HBreturnImag(hello){
 
 
-  function displayItemsForSale(items) {
-    var source = document.querySelector("#itemsForSale-Template").innerHTML;
-    var template = Handlebars.compile(source);
-    items.forEach(function(item){
-      var output = template(item);
-      ulElement.insertAdjacentHTML('beforeend', output);
-    });
-  }
-})();
+
+        }
+
+        function fetchJSONP(url, callback) {
+            var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+            var script = document.createElement('script');
+
+            window[callbackName] = function(data) {
+                delete window[callbackName];
+                document.body.removeChild(script);
+                callback(data);
+            };
+
+            script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+            document.body.appendChild(script);
+        }
+
+    })();
